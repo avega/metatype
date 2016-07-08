65360488e229391a84032120cfbae70ad0868a39
Vladimir V <avega.devel@yandex.ru>
2016-07-08 13:08:50 +0300

    Updated for linux, and tabs fixes.

--- src/metatype.cpp
+++ src/metatype.cpp
@@ -1,34 +1,35 @@
-#include <string>
+#include <cstdio>
+#include <cstring>
 
 class BaseType {
 public:
-	virtual ~BaseType() {};
-	virtual void getValue(char *ret) = 0;
+    virtual ~BaseType() {};
+    virtual void getValue(char *ret) = 0;
 };
 
 class CharType : public BaseType {
 public:
-	// using BaseType::getValue;
-	void getValue(char *ret);
+    // using BaseType::getValue;
+    void getValue(char *ret);
 private:
-	char value[255];
+    char value[255];
 };
 
 void CharType::getValue(char *ret) {
-	printf("CHAR BOOM!\n");
+    printf("CHAR BOOM!\n");
 
-	strcpy(value, "CHAR");
-	strcpy(ret, value);
+    strcpy(value, "CHAR");
+    strcpy(ret, value);
 }
 
 int main() {
-  printf("Test project\n");
+    printf("Test project\n");
 
-  BaseType *b = new CharType();
+    BaseType *b = new CharType();
 
-  char val[255];
-  b->getValue(val);
-  printf("%s\n", val);
+    char val[255];
+    b->getValue(val);
+    printf("%s\n", val);
 
-  return 0;
+    return 0;
 }
